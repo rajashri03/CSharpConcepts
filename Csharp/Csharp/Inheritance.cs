61ece5abeeffdc5fefa1a5921a2ef39fa4d7c42b
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-11 20:26:29 +0530

    Encapsulation

---
+++ Csharp/Csharp/Inheritance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    public class Inheritance  //Base Class
+    {
+        protected int a, b;
+        public void Adition()
+        {
+          
+            Console.WriteLine("Enter First Number:");
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Second Number:");
+            b = Convert.ToInt16(Console.ReadLine());
+
+        }
+    }
+    public class Add: Inheritance//Class Add – Derived Class
+    {
+        int Sum;
+        public void Adddata()
+        {
+          
+            Sum = a + b;
+            Console.WriteLine("Adition is:" + Sum);
+        }
+    }
+}
--- Csharp/Csharp/Program.cs
+++ Csharp/Csharp/Program.cs
@@ -1,5 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Csharp;
-Encapsulation num = new Encapsulation();
-num.Numtype = "10";
-Console.WriteLine("Given Number is:" + num.Numtype);
+bool flag = true;
+while(flag)
+{
+    Console.WriteLine("Select:\n1)Encapsulation\n2)Inheritance\n");
+    int op = Convert.ToInt16(Console.ReadLine());
+    switch(op)
+    {
+        case 1:
+            Encapsulation num = new Encapsulation();
+            num.Numtype = "10";
+            Console.WriteLine("Given Number is:" + num.Numtype);
+            break;
+        case 2:
+            Add output = new Add();
+            output.Adition();
+            output.Adddata();
+            break;
+        default:
+            flag = !flag;
+            break;
+    }
+     
+}
+
