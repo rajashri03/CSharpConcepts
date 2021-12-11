f91f7e1a7b1f50f20ba4af3cdb0f5d8e1427688d
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-11 19:48:30 +0530

    Encapsulation

---
+++ Csharp/Csharp/Encapsulation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    public class Encapsulation
+    {
+        string numType;
+        public string Numtype
+        {
+            get
+            {
+                return numType;
+            }
+            set
+            {
+                if(Convert.ToInt16(value)%2==0)
+                {
+                    numType = "Even";
+                }
+                else
+                {
+                    numType = "ODD";
+                }
+            }
+        }
+    }
+}
---
+++ Csharp/Csharp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Csharp;
+Encapsulation num = new Encapsulation();
+num.Numtype = "10";
+Console.WriteLine("Given Number is:" + num.Numtype);
